816ae1b576d08f5376f920b97cf22bc95618a776
m.khaleghi <mortezaprogram@gmail.com>
2023-01-26 16:24:03 +0330

    test

---
+++ tring_01.py
@@ -0,0 +1,11 @@
+secret_string=""
+norm_string="1rrrrr"
+for char in norm_string:
+    secret_string+=str(ord(char)-23)
+print(secret_string)
+norm_string=""
+for i in range(0,len(secret_string)-1,2):
+    norm_string+=chr(int(secret_string[i]+secret_string[i+1])+23)
+
+print(norm_string)
+

6a6ad4f91d497e688c654032f1034a9a2049030f
m.khaleghi <mortezaprogram@gmail.com>
2023-03-12 07:50:58 +0330

    test

--- iterable_test.py
+++ iterable_test.py
@@ -1,6 +1,43 @@
+'''
+samp_iter=iter("my_samplae")
+print("Char: ",next(samp_iter))
+print("Char: ",next(samp_iter))
+print("Char: ",next(samp_iter))
+print("Char: ",next(samp_iter))
+'''
+class Alphabet:
+    def __init__(self):
+       self.letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+       self.index=-1
 
-samp_iter=iter("my_ssaamplae")
-print("Char: ",next(samp_iter))
-print("Char: ",next(samp_iter))
-print("Char: ",next(samp_iter))
-print("Char: ",next(samp_iter))
+    def __iter__(self):
+       return self
+
+    def __next__(self):
+       if self.index >= len(self.letters)-1:
+           raise StopIteration
+       self.index+=1
+       return self.letters[self.index]
+alpha=Alphabet()
+for letter in alpha:
+    print(letter)
+
+Mori={"fname":"mori","lname":"XLG"}
+for key in Mori:
+    print(key,Mori[key])
+
+class FibGenerator:
+    def __init__(self):
+        self.first=0
+        self.second=1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        fib_num=self.first+self.second
+        self.first=self.second
+        self.second=fib_num
+        return fib_num
+
+fib_seq=FibGenerator()
+for i in range(10):
+    print("Fib: ",next(fib_seq))

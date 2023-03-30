5c4b3ac2169356b95082b0fce947da343dc4c16c
m.khaleghi <mortezaprogram@gmail.com>
2023-03-30 12:04:07 +0330

    test

--- tk_inter_02.py
+++ tk_inter_02.py
@@ -2,3 +2,12 @@ from tkinter import *
 from tkinter import ttk
 def set_entry(*args):
     entry_1_txt.set("Hello")
+def chk_but_changed(*args):
+    entry_1_txt.set(chk_but_1_txt.get())
+def radio_changed(*args):
+    entry_1_txt.set(entry_but_1_val.get())
+def combo_changed(*args):
+    entry_1_txt.set(combo_1_val.get())
+root=Tk()
+root.title("Widget Example")
+
